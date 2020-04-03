6f6c9da873e6d9bddfba56ea4415a63a7297373d
Parth Vyas <parthv700@gmail.com>
2020-04-03 17:33:20 +0530

    finally

--- my_answers.py
+++ my_answers.py
@@ -140,7 +140,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 1000
-learning_rate = 0.01
-hidden_nodes = 10
+iterations = 3000
+learning_rate = 0.3
+hidden_nodes = 13
 output_nodes = 1
